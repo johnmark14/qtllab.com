853f7bd2e7df352fd04c5862904b286285c89ab0
johnmark14 <johmarkgabrentina@gmail.com>
2021-02-07 15:12:51 +0800

    Attch datepicker widget to select option

--- assets/main-custom-theme.js
+++ assets/main-custom-theme.js
@@ -49,8 +49,10 @@ document.addEventListener('DOMContentLoaded', function() {
         showOn: "button",
         buttonImage: "https://cdn.shopify.com/s/files/1/0271/7220/8715/files/calendar.png?v=1612417406",
         buttonImageOnly: true,
-        yearRange: year+":"+yearLimit
+        yearRange: year+":"+yearLimit,
+        dateFormat: "m/d/yy"
     })
+
     // Initialize year option
     for(let i = 0; i < 3; i++) {
         $('#edit-submitted-pick-up-date-year').append($('<option>', {
@@ -75,6 +77,18 @@ document.addEventListener('DOMContentLoaded', function() {
         calendarApp._setYear($(this).children("option:selected").val());
         console.log("from object " + calendarApp._getFullDate());
         $("#txt-date").val(calendarApp._getFullDate());
+    });
+
+    $("#txt-date").change(function () {
+        let txtDateVal = $("#txt-date").val();
+        let dateArr = txtDateVal.split("/");
+        calendarApp._setMonth(dateArr[0]);
+        calendarApp._setDay(dateArr[1]);
+        calendarApp._setYear(dateArr[2]);
+
+        $("#edit-submitted-pick-up-date-month").val(calendarApp._getMonth()).change();
+        $("#edit-submitted-pick-up-date-day").val(calendarApp._getDay()).change();
+        $("#edit-submitted-pick-up-date-year").val(calendarApp._getYear()).change();
     });
 })
 
